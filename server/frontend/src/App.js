cd64fc89cf924a08b63888034374a8a4f0edde63
sabitbinazad <sabitbinazad01@gmail.com>
2025-01-03 09:56:53 -0500

    module 2 done

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,14 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Registration from "./components/Register/Register";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+
+      <Route path="/register" element={<Registration />} />
     </Routes>
   );
 }
